e52c655fab14d51f2c91cea756fb9099d5fb29d8
rshenoy-pc\rshenoy <rshenoy@bp-3.com>
2023-03-12 14:39:34 +0000

    Initial commit

---
+++ src/main/java/com/springbootwarwildfly/SpringbootWarWildflyApplication.java
@@ -0,0 +1,13 @@
+package com.springbootwarwildfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWarWildflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWarWildflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootwarwildfly/SpringbootWarWildflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootwarwildfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWarWildflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
